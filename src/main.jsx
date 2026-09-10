04aa7b2410d5612331fb7ab50d6aa2b4ad653870
Ritabrata-1911 <ritabrataroychowdhury@zohomail.in>
2026-09-11 00:28:51 +0530

    Co-authored-by: Souravhalder111 <Souravhalder111@users.noreply.github.com> Co-authored-by: git-sumit007 <git-sumit007@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import './i18n';
+import { registerSW } from 'virtual:pwa-register'
+registerSW({
+  immediate: true,
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,56 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'NeuroPlay',
+        short_name: 'NeuroPlay',
+
+        description:
+          'AI-based cognitive gaming and memory assistance platform for elderly users',
+
+        theme_color: '#2f5d50',
+        background_color: '#f7f4ed',
+
+        display: 'standalone',
+
+        start_url: '/',
+        scope: '/',
+
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+
+      workbox: {
+  globPatterns: [
+    '**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'
+  ],
+
+  maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+},
+    }),
+  ],
 })
